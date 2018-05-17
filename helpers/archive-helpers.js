25c673edc0139af810ca915b5de919b670a9e3a6
VincentCastelli <vincentsalvatorecastelli@gmail.com>
2018-05-16 20:00:43 -0700

    Pass GET requests tests.

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -26,15 +26,68 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  
 };
 
 exports.isUrlInList = function(url, callback) {
+  // if url !== url on sites.txt
+    // addUrlToList(url, readListOfUrls)
+    
+    
+  
+  fs.readFile(paths.list, (err, data) => {
+    //if url is in list
+    if(_.contains(data.toString().split('\n'), url)) {
+      fs.readFile(paths.siteAssets + '/loading.html', (err, data) => {
+        res.write(data);
+        res.end();
+      });
+    // if url is not in list, add to list
+    } else {
+      addUrlToList(url, callback); //ADD CALLBACK
+    }
+    
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  // get file
+  // convert to array
+  // push url to array
+  // convert array back to text
+  // write to list
+  // render loading page
+  fs.readFile(paths.list, (err, data) => {
+    var listArray = data.toString().split('\n');
+    listArray.push(url);
+    var listString = listArray.join('\n');
+    
+    fs.writeFile(paths.list, listString, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+
+    fs.readFile(paths.siteAssets + '/loading.html', (err, data) => {
+        res.write(data);
+        res.end();
+    });
+  });
 };
 
-exports.isUrlArchived = function(url, callback) {
+exports.isUrlArchived = function(url, callback) { 
+  // if url !== file in archivedSites
+  fs.readdir(paths.archivedSites, (err, files) => {
+    // if url in archive
+    if (_.some(files, file => file === url)) {
+      fs.readFile(paths.archivedSites + url, (err, data) => {
+        res.write(data);
+        res.end();
+      });
+    // if url not in archive
+    } else {
+      isUrlInList(url, callback); //ADD CALLBACK
+    }
+  }
 };
 
 exports.downloadUrls = function(urls) {
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,49 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var headersCor = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  // Get
+  var statusCode;
+  if (req.method === 'GET') {
+ 
+    // if url is '/'
+    if (req.url === '/') {
+      fs.readFile(archive.paths.siteAssets + '/index.html', (err, data) => {
+        if (err) {
+          statusCode = 404;
+          res.writeHead(statusCode, headersCor.headers);
+          res.end();
+        } else {
+          statusCode = 200;
+          res.writeHead(statusCode, headersCor.headers);
+          res.write(data);
+          res.end();
+        }
+    }) 
+    } else {
+      fs.readFile(archive.paths.archivedSites + req.url, (err, data) => {
+        if (err) {
+          statusCode = 404;
+          res.writeHead(statusCode, headersCor.headers);
+          res.end();
+        } else {
+          statusCode = 200;
+          res.writeHead(statusCode, headersCor.headers);
+          res.write(data);
+          res.end();
+        }
+      })
+    }  
+  }
+  
+  // Post
+  
+  if (req.method === 'POST') {
+    statusCode = 302;
+    res.writeHead(statusCode, headersCor.headers);
+  }
+  
 };
